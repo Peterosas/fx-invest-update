a9def9f1d6824dc514baa89541e2006352194986
Peter Osas <peter@airvend.ng>
2020-12-16 09:18:19 +0100

    change maintenance message

--- resources/views/backend/maintenance/index.blade.php
+++ resources/views/backend/maintenance/index.blade.php
@@ -5,7 +5,7 @@
 <div class = "row mt-3 mb-2">
     <div class = "col-md-12">
         <h1>Sorry, we're down for maintenance</h1>
-        <p>We'll be back shortly. Please check regularly for update!</p>
+        <p>We'll be back in two(2) weeks. Please check regularly for update!</p>
     </div>
 </div>	
         
