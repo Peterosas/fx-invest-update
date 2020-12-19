c659de4a893bd4b4c52d889731000e46c0025c85
Peter Osas <peter@airvend.ng>
2020-12-19 20:40:58 +0100

    Add public folder

---
+++ public/fxu/js/dashboard.js
@@ -0,0 +1,18 @@
+
+
+$(function () {
+
+  'use strict';
+	
+
+//data table
+    $('#example1').DataTable({
+      'paging'      : false,
+      'lengthChange': false,
+      'searching'   : false,
+      'ordering'    : true,
+      'info'        : false,
+      'autoWidth'   : false
+    });
+	
+}); // End of use strict
